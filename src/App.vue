6e1f9b699b6f0fcd80faed579f4c376cbf0a1915
Bogdan <bogdan7morozyuk@gmail.com>
2019-12-30 13:20:31 +0200

    chore(App): add sample project structure

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,60 @@
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import { Node } from "@/types/node";
 
 @Component
-export default class App extends Vue {}
+export default class App extends Vue {
+  node: Node = {
+    name: "vue-tree-view",
+    children: [
+      {
+        name: "public",
+        children: [
+          {
+            name: "index.html"
+          },
+          {
+            name: "favicon.ico"
+          }
+        ]
+      },
+      {
+        name: "src",
+        children: [
+          {
+            name: "assets",
+            children: [
+              {
+                name: "logo.png"
+              }
+            ]
+          },
+          {
+            name: "components",
+            children: [
+              {
+                name: "TreeView.vue"
+              }
+            ]
+          }
+        ]
+      },
+      {
+        name: "test",
+        children: [
+          {
+            name: "unit",
+            children: [
+              {
+                name: "example.spec.ts"
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  };
+}
 </script>
 
